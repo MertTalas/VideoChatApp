2d5aa899fbbd72bf17fee23e77c9a4c4fe713d82
Mert <merttalass@gmail.com>
2021-05-15 17:27:06 +0300

    SplashScreen eklendi

---
+++ app/src/main/java/com/impostors/videochatapp/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.impostors.videochatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2500;
+    private Animation topAnim,bottomAnim;
+    private ImageView image;
+    private TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageViewSplashScreenLogo);
+        logo = findViewById(R.id.textViewSplashDesc);
+        slogan = findViewById(R.id.textViewSplashScreenSlogan);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                splashIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
